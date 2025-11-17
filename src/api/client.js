db8aeee765364c7f7e92e04ac15bfe18f67a3b11
CesarlUISER <cr209187@gmail.com>
2025-11-17 00:35:25 -0600

    quinto commit

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const client = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:4000/api",
+  baseURL: process.env.REACT_APP_API_URL || "https://familycare-logistic-back.onrender.com/api",
 });
 
 // Adjunta Bearer token si existe
